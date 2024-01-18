f88b2607eb1000b908179af07289e615fa093958
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2024-01-18 17:29:26 +0100

    Fix linting

--- generate-markdown-doc/src/index.ts
+++ generate-markdown-doc/src/index.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { exec, execSync } from "child_process";
-import * as os from "node:os";
-import path from "path";
+import {exec, execSync} from 'child_process';
+import * as os from 'node:os';
+import path from 'path';
 import fs from 'node:fs';
 
 const targetRepo = process.argv[2];
@@ -10,20 +10,19 @@ const repoBasename = path.basename(targetRepo);
 const clonedRepoTmpDir = `${os.tmpdir()}/${repoBasename}`;
 
 // Be sure the repository does not exist
-fs.rmSync(clonedRepoTmpDir, { recursive: true, force: true });
+fs.rmSync(clonedRepoTmpDir, {recursive: true, force: true});
 
 console.log(`Target repository: ${targetRepo}`);
 
 execSync(`git clone ${targetRepo} ${clonedRepoTmpDir}`);
 
-const fileLst = fs.readdirSync(clonedRepoTmpDir, { recursive: true });
+const fileLst = fs.readdirSync(clonedRepoTmpDir, {recursive: true});
 const questionFiles = fileLst
   .filter(f => {
     const filename = path.basename(f.toString());
     return filename.startsWith('Q') && filename.endsWith('.txt');
   })
   .map(f => f.toString());
-
 
 const questionMap = new Map<string, string[]>();
 const answerMap = new Map<string, string[]>();
@@ -38,13 +37,15 @@ questionFiles.forEach(filePath => {
   if (filename.includes('-Explanation')) {
     const answerNum = filename.split('-')[0];
     const answer = fs.readFileSync(absolutePath, 'utf-8');
-    const answerLst = answerMap.get(title) ?? answerMap.set(title, []).get(title);
-    answerLst!!.push(`**${answerNum}.** ${answer}\n\n`);
+    const answerLst =
+      answerMap.get(title) ?? answerMap.set(title, []).get(title);
+    answerLst!.push(`**${answerNum}.** ${answer}\n\n`);
   } else {
     const questionNum = filename.split('.')[0];
     const question = fs.readFileSync(absolutePath, 'utf-8');
-    const questionLst = questionMap.get(title) ?? questionMap.set(title, []).get(title);
-    questionLst!!.push(`**${questionNum}.** ${question}\n\n`);
+    const questionLst =
+      questionMap.get(title) ?? questionMap.set(title, []).get(title);
+    questionLst!.push(`**${questionNum}.** ${question}\n\n`);
   }
 });
 
@@ -53,18 +54,29 @@ const markdownOutputDir = './markdownOutput';
 if (!fs.existsSync(markdownOutputDir)) {
   fs.mkdirSync(markdownOutputDir);
 }
-createMarkdownFile(markdownOutputDir, `${repoBasename}-questions.md`, questionMap);
+createMarkdownFile(
+  markdownOutputDir,
+  `${repoBasename}-questions.md`,
+  questionMap
+);
 createMarkdownFile(markdownOutputDir, `${repoBasename}-answers.md`, answerMap);
 
 // Clean up the folder
-fs.rmSync(clonedRepoTmpDir, { recursive: true, force: true });
+fs.rmSync(clonedRepoTmpDir, {recursive: true, force: true});
 
-
-function createMarkdownFile(dir: string, filename: string, dataMap: Map<string, string[]>): void {
-  fs.rmSync(filename, { force: true });
+function createMarkdownFile(
+  dir: string,
+  filename: string,
+  dataMap: Map<string, string[]>
+): void {
+  fs.rmSync(filename, {force: true});
 
   const output = fs.createWriteStream(path.join(dir, filename));
-  output.write(filename.includes('questions') ? `#QUESTIONS - ${filename}\n` : `#ANSWERS - ${filename}\n`);
+  output.write(
+    filename.includes('questions')
+      ? `#QUESTIONS - ${filename}\n`
+      : `#ANSWERS - ${filename}\n`
+  );
 
   dataMap.forEach((valueLst, title) => {
     output.write(title);
@@ -73,7 +85,6 @@ function createMarkdownFile(dir: string, filename: string, dataMap: Map<string, 
 
   console.log(`The file: ${filename} has been created...`);
 
-
   output.end();
 }
 
@@ -81,8 +92,7 @@ function buildTitle(filePath: string, filename: string): string {
   const lst = filePath.replace(filename, '').split('/');
   let markdownLevel = 2;
   let title = '';
-  lst.forEach(value => title += '#'.repeat(markdownLevel++) + value + '\n');
+  lst.forEach(value => (title += '#'.repeat(markdownLevel++) + value + '\n'));
 
   return title;
 }
-
